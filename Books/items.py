7194fb184a29111b9e72dd2ec9842e9f3430437b
sean <yomingyo@gmail.com>
2020-12-22 17:35:38 +0800

    use item

--- Books/items.py
+++ Books/items.py
@@ -9,4 +9,6 @@ import scrapy
 class BooksItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    section = scrapy.Field()
+    text_info = scrapy.Field()
+    content = scrapy.Field()
--- Books/spiders/stroy.py
+++ Books/spiders/stroy.py
@@ -7,26 +7,23 @@ class StroySpider(scrapy.Spider):
     start_urls = ['https://www.biquge9.cc/book/18220312/672306968.html']
 
     def parse(self, response):
+        item = BooksItem()
         def text_to_string(list):
             # s = str(list)
             # p = r'[\u4e00-\u9fa5]+|[，]+|[。]' # 匹配汉字，逗号
             # r = re.findall(p, s)
             return ''.join(list)
-        section = response.css('div.content h1::text').get()
-        textinfo = response.css('div.textinfo span::text').getall()
-        content = text_to_string(response.css('div.showtxt::text').getall())
+        item['section'] = response.css('div.content h1::text').get()
+        item['text_info'] = response.css('div.textinfo span::text').getall()
+        item['content'] = text_to_string(response.css('div.showtxt::text').getall())
         next_section = response.css('div.page_chapter a::attr(href)').getall()[2]
-        yield {
-            'section': section,
-            'textinfo': textinfo,
-            'content': content,
-        }
 
+        yield item
         # with open('aa.txt', 'w') as f: # 这样写入会一直打开关闭 覆盖之前的内容 是不可取的， 可能要在管道里边操作
         #   f.write(section)
         #   f.write(content)
 
-        if next_section is not None:
-            yield scrapy.Request(next_section, callback=self.parse)
+        # if next_section is not None:
+        #     yield scrapy.Request(next_section, callback=self.parse)
 
 
